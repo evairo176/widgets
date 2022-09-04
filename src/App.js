473ce4b795b15843c580a2f7a5c30af8f8e5bf5d
Dicki Prasetya <semenjakpetang176@gmail.com>
2022-09-05 01:56:43 +0700

    belajar hook

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React from "react";
+import Accordion from "./componnents/Accordion";
+import Search from "./componnents/Search";
 
+const items = [
+  {
+    title: "1. What is React?",
+    content: "React is front end javascript framework",
+  },
+  {
+    title: "2. Why use React?",
+    content: "React is favorite JS library among enginners",
+  },
+  {
+    title: "2. How do you use React?",
+    content: "You use React by creating componnnent",
+  },
+];
 class App extends React.Component {
   render() {
-    return <div>hi tayo</div>;
+    return (
+      <div>
+        {/* <Accordion items={items} /> */}
+        <Search />
+      </div>
+    );
   }
 }
 
