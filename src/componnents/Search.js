26986236fb10b5fa71be081bde59c3323346fc2e
Dicki Prasetya <semenjakpetang176@gmail.com>
2022-09-06 00:45:20 +0700

    membuat api search wikipedia

--- src/componnents/Search.js
+++ src/componnents/Search.js
@@ -1,7 +1,52 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Search = () => {
   const [term, setTerm] = useState("");
+  const [result, setResult] = useState([]);
+
+  console.log(result);
+  useEffect(() => {
+    const search = async () => {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term,
+        },
+      });
+      setResult(data.query.search);
+    };
+
+    setTimeout(() => {
+      if (term) {
+        search();
+      } else {
+        setResult([]);
+      }
+    }, 500);
+  }, [term]);
+
+  const renderResult = result.map((result) => {
+    return (
+      <div key={result.pageid} className="item">
+        <div className="right floated content">
+          <a
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+            className="ui button"
+          >
+            Go
+          </a>
+        </div>
+        <div className="content">
+          <div className="header">{result.title}</div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+        </div>
+      </div>
+    );
+  });
   return (
     <div>
       <div className="ui form">
@@ -13,6 +58,7 @@ const Search = () => {
             className="input"
           />
         </div>
+        <div className="ui celled list">{renderResult}</div>
       </div>
     </div>
   );
